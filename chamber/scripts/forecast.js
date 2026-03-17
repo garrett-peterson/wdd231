34ad81eb96f7cdec8f8d9c3e51b9af04db576c9c
garrett-peterson <garrett.peterson726@gmail.com>
2026-03-17 16:25:17 -0500

    Did the weather sections for my home page

---
+++ chamber/scripts/forecast.js
@@ -0,0 +1,44 @@
+const tomorrowTemp = document.querySelector('#tomTemp');
+const dayAfterTemp = document.querySelector('#dayAfterTemp');
+
+const tomorrow = document.querySelector('#tomorrow');
+const dayAfter = document.querySelector('#dayAfter');
+
+const key = "6dae3c6e1013f342fa9f2f8a6c207d66";
+const lat = "33.29";
+const long = "-96.59";
+
+const forecastURL = `//api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${key}&units=imperial`;
+
+async function forecastFetch() {
+  try {
+    const response = await fetch(forecastURL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayForecast(data); 
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function displayForecast(data) {
+    const twoDayForecast = data.list.filter(item =>
+        item.dt_txt.includes("12:00:00")).slice(0, 2);
+    console.log(twoDayForecast);
+
+    tomorrowTemp.innerHTML = `${twoDayForecast[0].main.temp_max}&deg;F`;
+    dayAfterTemp.innerHTML = `${twoDayForecast[1].main.temp_max}&deg;F`;
+
+    tomorrow.innerHTML = new Date(twoDayForecast[0].dt_txt).toLocaleDateString([], {weekday: "long"});
+
+    dayAfter.innerHTML = new Date(twoDayForecast[1].dt_txt).toLocaleDateString([], {weekday: "long"});
+
+}
+
+
+
+forecastFetch();
---
+++ chamber/scripts/weather.js
@@ -0,0 +1,60 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#condition');
+const tempHigh = document.querySelector('#high');
+const tempLow = document.querySelector('#low');
+const humidity = document.querySelector('#humid');
+const sunrise = document.querySelector('#rise');
+const sunset = document.querySelector('#set');
+
+const todayTemp = document.querySelector('#todayTemp');
+
+
+const myKey = "6dae3c6e1013f342fa9f2f8a6c207d66";
+const myLat = "33.29";
+const myLong = "-96.59";
+
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); 
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+function displayResults(data) {
+    todayTemp.innerHTML = `${data.main.temp_max}&deg;F`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute('src', iconsrc);
+    captionDesc.textContent = `${desc}`;
+
+    tempHigh.innerHTML = `${data.main.temp_max}&deg;`;
+    tempLow.innerHTML = `${data.main.temp_min}&deg;`;
+    humidity.textContent = data.main.humidity;
+
+    sunrise.innerHTML = `${new Date(data.sys.sunrise * 1000).toLocaleTimeString([], {
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true
+        })}`;
+    sunset.innerHTML = `${new Date(data.sys.sunset * 1000).toLocaleTimeString([], {
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true
+        })}`;
+}
+
+apiFetch();
